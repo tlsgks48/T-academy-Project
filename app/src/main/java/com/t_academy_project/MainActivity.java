42b3ed16c6116b8fd514f3f23a3e8c8663610107
tlsgks48 <tlsgks486@gmail.com>
2020-11-09 21:44:42 +0900

    ListView 적용

--- app/src/main/java/com/t_academy_project/MainActivity.java
+++ app/src/main/java/com/t_academy_project/MainActivity.java
@@ -3,12 +3,59 @@ package com.t_academy_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView selected_item_textview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listview = (ListView)findViewById(R.id.listview);
+        selected_item_textview = (TextView)findViewById(R.id.selected_item_textview);
+
+
+        //데이터를 저장하게 되는 리스트
+        List<String> list = new ArrayList<>();
+
+        //리스트뷰와 리스트를 연결하기 위해 사용되는 어댑터
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, list);
+
+        //리스트뷰의 어댑터를 지정해준다.
+        listview.setAdapter(adapter);
+
+
+        //리스트뷰의 아이템을 클릭시 해당 아이템의 문자열을 가져오기 위한 처리
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> adapterView,
+                                    View view, int position, long id) {
+
+                //클릭한 아이템의 문자열을 가져옴
+                String selected_item = (String)adapterView.getItemAtPosition(position);
+
+                //텍스트뷰에 출력
+                selected_item_textview.setText(selected_item);
+            }
+        });
+
+
+        //리스트뷰에 보여질 아이템을 추가
+        list.add("사과");
+        list.add("배");
+        list.add("귤");
+        list.add("바나나");
     }
 }
